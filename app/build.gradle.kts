0467d0c727eaef94aacdb1b8d3dd46f676afc16c
Palt <fakerzx98@gmail.com>
2024-01-06 11:07:18 +0300

    step 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    application
 }
 
 group = "hexlet.code"
@@ -9,9 +12,19 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("hexlet.code.App")
+    }
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    }
+
+tasks {
+    compileJava {
+        options.release = 20
+    }
 }
 
 tasks.test {
