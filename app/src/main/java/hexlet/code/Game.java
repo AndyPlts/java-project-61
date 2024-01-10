b01f9859fbd9f49cf124ffe8fbd18cf641379fc6
Palt <fakerzx98@gmail.com>
2024-01-10 19:13:26 +0300

    step 5

--- app/src/main/java/hexlet/code/Game.java
+++ app/src/main/java/hexlet/code/Game.java
@@ -5,29 +5,36 @@ import java.util.Scanner;
 public class Game {
 
     public static void isEven() {
-        int counter = 0;
-        int minValue = 1;
-        int maxValue = 1000;
+        
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = scanner.next();
         System.out.println("Hello, " + name + "!");
-
         System.out.println("Answer 'yes' if the number is even,"
                 + "otherwise answer 'no'.");
 
+        playIsEven(scanner, name);
+
+        System.out.println("Congratulations, " + name + "!");
+
+        scanner.close();
+    }
+
+    private static void playIsEven(Scanner scanner, String name) {
+        int counter = 0;
+        int minValue = 1;
+        int maxValue = 1000;
         while (counter != 3) {
             int number = minValue + (int) (Math.random() * (maxValue - minValue + 1));
             System.out.println("Question: " + number);
             System.out.print("Your answer: ");
             String answer = scanner.next();
             if ((number % 2 == 0) && (answer.equals("yes"))) {
-                System.out.println("Correct");
+                System.out.println("Correct!");
                 counter++;
             } else if ((number % 2 != 0) && (answer.equals("no"))) {
-                System.out.println("Correct");
+                System.out.println("Correct!");
                 counter++;
             } else if (number % 2 == 0) {
                 System.out.println("'" + answer + "' is wrong answer "
@@ -41,9 +48,5 @@ public class Game {
                 counter = 0;
             }
         }
-
-        System.out.println("Congratulations, " + name + "!");
-
-        scanner.close();
     }
 }
